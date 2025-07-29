b7e092dca192103f8b937e659da122e1caf26003
Mihaly Ale <105678319+mihaly-ale@users.noreply.github.com>
2025-07-29 20:38:11 +0200

    Setup template for project

---
+++ src/main.js
@@ -0,0 +1,116 @@
+// Styles
+import '../src/styles/scss/main.scss';
+
+// Assets
+import devLogo from '/ma.svg';
+import socialIconSprite from '/src/assets/social-icon-sprite.svg';
+const cardContents = [
+	{
+		title: 'Sass',
+		desc: 'The template uses Sass for styling. It includes variables, mixins, and partials to speed up development and keep styles consistent. For font it uses <a href="https://utopia.fyi/" target="_blank">Utopia - Clamp Calculator</a>. The setup is easy to customize, making it simple to adapt the look to any project. For full description check the github repo, for the included reset file see the repo also on GitHub.',
+	},
+	{
+		title: 'Vite',
+		desc: ' This template is the customized version of the basic Vite preset. Vite provides lightning-fast hot module replacement (HMR) and near-instant startup. Vite also optimizes your code for production with minimal configuration. Supports many framework, including React, Vue, Svelte and many more.',
+	},
+	{
+		title: 'Javascript',
+		desc: ' The template uses modern ES6 JavaScript without relying on a framework. This keeps the bundle light and the learning curve low for those who know vanilla JS. It gives full control over behavior without extra abstraction or dependencies.  ',
+	},
+];
+const socialLinks = [
+	'youtube',
+	'x',
+	'vimeo',
+	'spotify',
+	'soundcloud',
+	'pinterest',
+	'patreon',
+	'linkedin',
+	'instagram',
+	'github',
+	'facebook',
+	'dribbble',
+	'codepen',
+];
+
+// Components
+import { setupCounter } from './components/counter.js';
+import createCard from './components/Card.js';
+import createSocialLink from './components/SocialLink.js';
+
+document.querySelector('#app').innerHTML = `
+  <header class="flex-center full-bleed">
+    <div class="container-header-logo">
+      <img src="/__logos#sprite-logomark-dark-view" class="logo" alt="" width="80" height="80"/>
+      <img src="/__logos#sprite-logomark-light-view" class="logo" alt="" width="80" height="80"/>
+      <img src="/__logos#sprite-wordmark-dark-view" class="logo" alt="" width="160" height="20"/>
+      <img src="/__logos#sprite-wordmark-light-view" class="logo" alt="" width="160" height="20"/>
+
+    </div>
+
+    <h1 class="center-text">Vite + Javascript</h1>
+    <button  id="counter" type="button" class="btn"></button>
+
+    <p class="center-text">
+      Click on the Vite logo to learn more
+    </p>
+  </header>
+
+  <main>
+    <section class="cards">
+    </section>
+    <blockquote class="flex-center">
+      <p>"There is always one more bug to fix ... Society may want to put its trust in computers, but it should know the facts: a bug, fix it. Another bug, fix it. The 'fix' itself may introduce a new bug. And so on."</p>
+      <cite class="center-text">Ellen Ullman, <i>Close to the Machine: Technophilia and Its Discontents</i>.</cite>
+    </blockquote>
+  </main>
+
+  <footer class="full-bleed">
+    <section class="button-container flex-center">
+
+      <button type="button" class="btn-primary">Button</button>
+      <button type="button" class="btn-secondary">Button</button>
+      <button type="button" class="btn-tertiary">Button</button>
+      <button type="button" class="btn-secondary-light">Button</button>
+
+
+      <svg class="icon">
+        <use xlink:href="/__icons#sprite-book-fill"></use>
+      </svg>
+    </section>
+
+    <div class="social-icons">
+    </div>
+
+
+    <p>
+      Template made by
+      <a href="https://github.com/mihaly-ale">
+        <img src="${devLogo}" class="dev-logo" width="32" height="32"/>
+      </a>
+    </p>
+
+
+
+
+
+
+
+  </footer>
+`;
+
+setupCounter(document.querySelector('#counter'));
+
+const cardGrid = document.querySelector('.cards');
+
+cardContents.forEach((content) => {
+	const card = createCard({ title: content.title, text: content.desc });
+	cardGrid.appendChild(card);
+});
+
+const SocialIcons = document.querySelector('.social-icons');
+socialLinks.forEach((item) => {
+	const link = createSocialLink(socialIconSprite, item);
+	SocialIcons.appendChild(link);
+});
